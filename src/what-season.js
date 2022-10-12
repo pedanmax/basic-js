b85570df965c2527c8913e47c4e6551764790a20
pedanmax <m.pedan2017@gmail.com>
2022-10-12 11:43:15 +0300

    feat:add mine-sweeper task

--- src/what-season.js
+++ src/what-season.js
@@ -11,40 +11,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
- function getSeason(date) {
-  try{
-     let isDate 
-     isDate = Date.parse(date)
-        if (date === undefined){
-           return 'Unable to determine the time of year!'
-        } 
-        // if (isNaN(isDate)){
-        //    throw new Error("Invalid date!")
-        // }
-     let prototype = Object.keys(date)
-        if(prototype.length > 0){
-              throw new Error("Invalid date!")
-           }
-     let month = date.getMonth()
-        if ((month <= 1 || month == 11)){
-           return 'winter'
-        }
-        if ( (month >= 2 && month < 5)){
-           return 'spring'
-        }
-        if ( (month >= 5 && month < 8)){
-           return 'summer'
-        }
-        if ( (month >= 8 && month < 11)){
-           return 'autumn'
-        }
-  }
-  catch(error){
-     throw new Error("Invalid date!")
-  }
-  if (date.PrototypeOf){
-     throw new Error("Invalid date!")
-  }
+function getSeason(date) {
+   try{
+      let isDate 
+      isDate = Date.parse(date)
+         if (date === undefined){
+            return 'Unable to determine the time of year!'
+         } 
+         // if (isNaN(isDate)){
+         //    throw new Error("Invalid date!")
+         // }
+      let prototype = Object.keys(date)
+         if(prototype.length > 0){
+               throw new Error("Invalid date!")
+            }
+      let month = date.getMonth()
+         if ((month <= 1 || month == 11)){
+            return 'winter'
+         }
+         if ( (month >= 2 && month < 5)){
+            return 'spring'
+         }
+         if ( (month >= 5 && month < 8)){
+            return 'summer'
+         }
+         if ( (month >= 8 && month < 11)){
+            return 'autumn'
+         }
+   }
+   catch(error){
+      throw new Error("Invalid date!")
+   }
+   if (date.PrototypeOf){
+      throw new Error("Invalid date!")
+   }
 }
 
 module.exports = {
