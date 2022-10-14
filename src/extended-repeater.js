0894cbc51ce8572509fa4df705b8ea7533ed991a
pedanmax <m.pedan2017@gmail.com>
2022-10-14 21:16:27 +0300

    feat:add repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -9,15 +9,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  *
  * @example
+
  * 
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let repeatTimes = options.repeatTimes || 1;
+  let addition = options.addition
+  let additionRepeat = options.additionRepeatTimes || 1;
+  let separator = options.separator || '+';
+  let additionSeparator = options.additionSeparator || '|';
+  let result = '';
+  let preResult = '';
+  
+  typeof addition === 'boolean' || addition === null ? addition = String(addition) : addition = (addition) || '';
+
+  for (let i = 0; i < additionRepeat; i++) {
+      i !== additionRepeat - 1 ? preResult += addition + additionSeparator : preResult += addition;
+  }
+
+  for (let i = 0; i < repeatTimes; i++) {
+      i !== repeatTimes - 1 ? result += str + preResult + separator : result += str + preResult;
+  }
+  return result;
 }
 
 module.exports = {
